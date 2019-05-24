86b969e117456d1c42ec968574a6482bd2f5eed2
Remy93130 <remy93130@gmail.com>
2019-05-24 17:13:27 +0200

    About view + main

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    /**
+     * @Route("/base", name="base")
+     */
+    public function index()
+    {
+        return $this->render('base/index.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about()
+    {
+        return $this->render('base/about.html.twig');
+    }
+}
