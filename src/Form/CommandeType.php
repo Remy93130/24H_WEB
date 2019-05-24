28611b1bdb6ba375703ac09b4f676b6d25de7f06
Remy93130 <remy93130@gmail.com>
2019-05-24 19:39:09 +0200

    select 2 commande

--- src/Form/CommandeType.php
+++ src/Form/CommandeType.php
@@ -7,6 +7,7 @@ use App\Entity\Commande;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,18 +17,20 @@ class CommandeType extends AbstractType
     {
         $builder
             ->add('date', DateTimeType::class, [
+                "label" => false,
                 "attr" => ['hidden' => true]
             ])
-            ->add('etat')
-            ->add('quantite')
+            ->add('etat', HiddenType::class)
             // ->add('acheteur')
             // ->add('vendeur')
             ->add('cafe', EntityType::class, [
                 'class' => Cafe::class,
                 'choice_label' => function (Cafe $cafe) {
-                    return $cafe->getNom() . " / " . $cafe->getProprietaire()->getEntreprise();
+                    return $cafe->getNom() . " | " . $cafe->getProprietaire()->getEntreprise();
                 }
             ])
+            ->add('quantite')
+
         ;
     }
 
