a16c425c94c469bce1ada634cb360b17cb806779
LinKinoMalandrin <39130976+LinKinoMalandrin@users.noreply.github.com>
2019-05-24 17:57:34 +0200

    Forms done

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class BaseController extends AbstractController
 {
     /**
-     * @Route("/base", name="base")
+     * @Route("/", name="index")
      */
     public function index()
     {
